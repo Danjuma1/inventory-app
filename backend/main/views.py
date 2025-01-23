1f7e589fdefc7b27b7d56897de730d78753d42a1
Danjuma1 <hda0202@gmail.com>
2025-01-23 11:41:31 +0100

    write views for item

--- backend/main/views.py
+++ backend/main/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Item
+from .serializers import ItemSerializer
 
-# Create your views here.
+class ItemViewSet(viewsets.ModelViewSet):
+    """
+    A simple ViewSet for managing inventory items (CRUD).
+    """
+    queryset = Item.objects.all()
+    serializer_class = ItemSerializer
