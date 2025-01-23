0ae74fea66182e94de8e53fc8d9af42982b95ce4
Danjuma1 <hda0202@gmail.com>
2025-01-23 11:39:49 +0100

    built item model

--- backend/main/models.py
+++ backend/main/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+class Item(models.Model):
+    CATEGORY_CHOICES = [
+        ('Electronics', 'Electronics'),
+        ('Furniture', 'Furniture'),
+        ('Clothing', 'Clothing'),
+        ('Other', 'Other'),
+    ]
+
+    name = models.CharField(max_length=100)
+    quantity = models.PositiveIntegerField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    category = models.CharField(max_length=50, choices=CATEGORY_CHOICES)
+
+    def __str__(self):
+        return self.name
